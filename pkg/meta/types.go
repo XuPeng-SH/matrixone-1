08c7de751debf4c1b7599a6b5a8046153ee83c83
XuPeng-SH <xupeng3112@163.com>
2023-07-10 13:07:31 +0800

    update

--- pkg/meta/types.go
+++ pkg/meta/types.go
@@ -10,6 +10,19 @@ import (
 	"github.com/tidwall/btree"
 )
 
+// For each table, it maitains table table as object list
+// 1. There are two types of objects: In-memory object and on-disk object
+//    On-Disk:     [Object1, Object2, Object3, Object4, Object5]
+//    In-memory:   [Object6, Object7, Object8, Object9, Object10]
+// 2. In-memory object will finally be flushed to be on-disk object
+//    On-Disk:     [Object1, Object2, Object3, Object4, Object5, Object6]
+//    In-memory:   [Object7, Object8, Object9, Object10]
+// 3. There is only one active in-memory object for each table. All in-active
+//    in-memory objects should be flushed to be on-disk objects
+//    In-memory:   [Object7, Object8, Object9, Object10]
+//                                                |
+//												  +-----> Active In-memory Object
+
 type AppendNode struct {
 	txnbase.TxnMVCCNode
 	chain    *AppendChain
