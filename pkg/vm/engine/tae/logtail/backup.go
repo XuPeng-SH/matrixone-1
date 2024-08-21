8cfc5b44aad08c88fef8a4272748b7a0f4d7f856
GreatRiver <great.river703@gmail.com>
2024-08-21 19:31:08 +0800

    Update code

--- pkg/vm/engine/tae/logtail/backup.go
+++ pkg/vm/engine/tae/logtail/backup.go
@@ -620,8 +620,8 @@ func ReWriteCheckpointAndBlockFromKey(
 				}
 				files = append(files, name.String())
 				blockLocation = objectio.BuildLocation(name, extent, blocks[0].GetRows(), blocks[0].GetID())
-				//objectio.SetObjectStatsLocation(objectData.stats, blockLocation)
 				objectData.stats = &writer.GetObjectStats()[objectio.SchemaData]
+				objectio.SetObjectStatsLocation(objectData.stats, blockLocation)
 				logutil.Infof("delete object %v len blk %v is not  0, row is %v ,extent is %v, dataBlocks is %d", objectData.stats.ObjectLocation().String(), blockLocation.String(), blocks[0].GetRows(), extent.String(), len(dataBlocks))
 				if insertObjBatch[objectData.tid] == nil {
 					insertObjBatch[objectData.tid] = &iObjects{
