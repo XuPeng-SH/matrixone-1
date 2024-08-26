40c65012ff5b0f196c85c6ee141a471b8d0220d9
GreatRiver <great.river703@gmail.com>
2024-08-26 15:07:27 +0800

    Fix error

--- pkg/vm/engine/disttae/logtailreplay/partition_state_inprogress.go
+++ pkg/vm/engine/disttae/logtailreplay/partition_state_inprogress.go
@@ -28,7 +28,6 @@ import (
 	"github.com/matrixorigin/matrixone/pkg/pb/api"
 	"github.com/matrixorigin/matrixone/pkg/perfcounter"
 	txnTrace "github.com/matrixorigin/matrixone/pkg/txn/trace"
-	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/blockio"
 	"github.com/tidwall/btree"
 	"math"
 	"runtime/trace"
